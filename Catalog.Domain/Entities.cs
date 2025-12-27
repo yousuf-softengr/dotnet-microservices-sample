bf66be8557c6d5c0b688cf11e4876b376f9a0d81
Muhammad Abu Yousof <yousuf.soft.engr@gmail.com>
2025-12-27 21:24:40 +0600

    Delete Catalog.Domain/Entities.cs

--- Catalog.Domain/Entities.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Catalog.Domain.Entities;
-
-public class Product
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = default!;
-    public decimal Price { get; set; }
-}
