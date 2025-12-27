41b0fcf8265afce28b237dc9908c5d42d23ee454
yousuf-softengr <yousuf.soft.engr@gmail.com>
2025-12-27 21:07:55 +0600

    Add Catalog API endpoint

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[]
+            {
+            new { Id = 1, Name = "Laptop", Price = 1200 },
+            new { Id = 2, Name = "Mouse", Price = 20 }
+        });
+        }
+    }
+}
