e4d2cf65f7e3698bade179d7bfa68584a1a24232
yousuf-softengr <yousuf.soft.engr@gmail.com>
2025-12-27 21:26:59 +0600

    Add Catalog.Domain layer with Product entity

---
+++ Catalog.Domain/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+}
