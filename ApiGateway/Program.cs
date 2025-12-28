1b0c8e63fddb6223f1ee92cdbebfcd785eb43ff3
yousuf-softengr <yousuf.soft.engr@gmail.com>
2025-12-28 21:34:04 +0600

    Configure Ocelot routing for Docker environment

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,6 +1,10 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
